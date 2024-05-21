a1ae52ea9c873dd98459a4a3c86e98c53549dbe0
fxcx <87233669+fxcx@users.noreply.github.com>
2024-05-21 14:11:15 -0300

    diseño 3

---
+++ 03/js/custom.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	"use strict";
+		
+			$('.brand-item').owlCarousel({
+				items:6,
+				loop:true,
+				smartSpeed: 1000,
+				autoplay:true,
+				dots:false,
+				autoplayHoverPause:false,
+				responsive:{
+						0:{
+							items:2
+						},
+						415:{
+							items:2
+						},
+						600:{
+							items:3
+						},
+						1000:{
+							items:6
+						}
+					}
+				});
+				
+				
+				$('.play').on('click',function(){
+					owl.trigger('play.owl.autoplay',[1000])
+				})
+				$('.stop').on('click',function(){
+					owl.trigger('stop.owl.autoplay')
+				})
+
+});
+
+
